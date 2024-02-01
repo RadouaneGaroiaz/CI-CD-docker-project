a51e9990eb66fec0316ef6243af8a5aed469ca42
RadouaneGaroiaz <garoiaz.radouane@gmail.com>
2024-02-01 14:52:52 +0100

    Add Swagger documentation for API endpoints

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,3 +1,125 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the product
+ *         name:
+ *           type: string
+ *           description: The name of your product
+ *         price:
+ *           type: string
+ *           description: The price of the product 
+ *     
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ * /products:
+ *   get:
+ *     summary: Lists all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The created product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ * /products/{id}:
+ *   get:
+ *     summary: Get the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ *   put:
+ *    summary: Update the product by the id
+ *    tags: [Products]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Product'
+ *    responses:
+ *      200:
+ *        description: The product was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      404:
+ *        description: The product was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *
+ *     responses:
+ *       200:
+ *         description: The product was deleted
+ *       404:
+ *         description: The product was not found
+ */
+
 const express = require('express');
 const router = express.Router();
 
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const createError = require('http-errors');
 const dotenv = require('dotenv').config();
+//swagger
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express");
+
 //allow any cors
 
 const cors = require('cors');
@@ -23,16 +27,43 @@ require('./initDB')();
 const ProductRoute = require('./Routes/Product.route');
 app.use('/products', ProductRoute);
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "my Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple products API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Radouane Garoiaz",
+        url: "radouane.garoiaz@gmail.com",
+        email: "radouane.garoiaz@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
 //404 handler and pass to error handler
-app.use((req, res, next) => {
+//app.use((req, res, next) => {
   /*
   const err = new Error('Not found');
   err.status = 404;
   next(err);
   */
   // You can use the above code if your not using the http-errors module
-  next(createError(404, 'Not found'));
-});
+ // next(createError(404, 'Not found'));
+//});
 
 //Error handler
 app.use((err, req, res, next) => {
@@ -44,6 +75,13 @@ app.use((err, req, res, next) => {
     }
   });
 });
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 const PORT = process.env.PORT || 3000;
 
